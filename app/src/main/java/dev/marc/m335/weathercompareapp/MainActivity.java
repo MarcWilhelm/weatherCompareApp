4293cdab3ef76e9dd0b0adb7e37546e87c919f88
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2024-01-10 14:37:34 +0100

    added a disabled Button

--- app/src/main/java/dev/marc/m335/weathercompareapp/MainActivity.java
+++ app/src/main/java/dev/marc/m335/weathercompareapp/MainActivity.java
@@ -2,7 +2,11 @@ package dev.marc.m335.weathercompareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nextBtn = (Button)findViewById(R.id.nextButton);
+        TextInputLayout nameInput = (TextInputLayout) findViewById(R.id.nameInput);
+       if (nameInput.getEditText().getText().toString().matches("")){
+           nextBtn.setEnabled(false);
+       }
+       else {
+           nextBtn.setEnabled(true);
+       }
+
+
+
+
+        Intent i = new Intent(MainActivity.this, CompareActivity.class);
+        i.putExtra("name", "Marc");
+        startActivity(i);
+
     }
 }
