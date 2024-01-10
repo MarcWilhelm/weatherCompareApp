7a24c86b1a56b2867fcb74e889b4a8dab36f54df
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2024-01-10 15:19:08 +0100

    sett button disabled on the start and make active if input was given

--- app/src/main/java/dev/marc/m335/weathercompareapp/MainActivity.java
+++ app/src/main/java/dev/marc/m335/weathercompareapp/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -14,21 +18,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button nextBtn = (Button)findViewById(R.id.nextButton);
-        TextInputLayout nameInput = (TextInputLayout) findViewById(R.id.nameInput);
-       if (nameInput.getEditText().getText().toString().matches("")){
-           nextBtn.setEnabled(false);
-       }
-       else {
-           nextBtn.setEnabled(true);
-       }
+        Button nextBtn = (Button) findViewById(R.id.nextButton);
+        TextInputLayout nameInput = (TextInputLayout) findViewById(R.id.name);
+        nameInput.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().trim().length()==0){
+                    nextBtn.setEnabled(false);
+                } else {
+                    nextBtn.setEnabled(true);
+                }
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
 
 
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CompareActivity.class);
+                i.putExtra("name", "Marc");
+                startActivity(i);
+            }
+        });
 
-        Intent i = new Intent(MainActivity.this, CompareActivity.class);
-        i.putExtra("name", "Marc");
-        startActivity(i);
 
     }
 }
