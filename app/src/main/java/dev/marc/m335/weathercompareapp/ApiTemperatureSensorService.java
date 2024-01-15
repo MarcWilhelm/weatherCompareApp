e63c083579dba5ab09191fe84a2d0f9ac9b887fd
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2024-01-15 08:58:16 +0100

    get Temperature from sensor

---
+++ app/src/main/java/dev/marc/m335/weathercompareapp/ApiTemperatureSensorService.java
@@ -0,0 +1,16 @@
+package dev.marc.m335.weathercompareapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ApiTemperatureSensorService extends Service {
+    public ApiTemperatureSensorService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- app/src/main/java/dev/marc/m335/weathercompareapp/TemperatureSensorService.java
+++ app/src/main/java/dev/marc/m335/weathercompareapp/TemperatureSensorService.java
@@ -1,42 +1,85 @@
 package dev.marc.m335.weathercompareapp;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TemperatureSensorService extends Service {
+
+public class TemperatureSensorService extends Service implements SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private Sensor ambientTemperatureSensor;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        ambientTemperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
+        if (ambientTemperatureSensor != null) {
+            sensorManager.registerListener(this, ambientTemperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            sendTemperatureBroadcast(25);
+        } else {
+            Toast.makeText(this, "Temperature Sensor Nicht Ereichbar oder Vorhanden", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void sendTemperatureBroadcast(float temperature) {
+        Intent intent = new Intent("updateTemp");
+        intent.putExtra("TEMPERATURE", temperature);
+        sendBroadcast(intent);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            float temperature = event.values[0];
+            sendTemperatureBroadcast(temperature); // Send temperature updates as they change
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Unregister sensor listener
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
     public TemperatureSensorService() {
     }
 
     private final IBinder binder = new LocalBinder();
-
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Double height = intent.getExtras().getDouble("height");
-        Double weight = intent.getExtras().getDouble("weight");
-        // return START_STICKY;$
-        System.out.println(weight);
-        System.out.println(height);
-
-        return flags;
-
-    }
-
-    public static Integer getTemperatureFromSensor() {
-        System.out.println("Gets Executed");
-        return -5;
-    }
 
     public class LocalBinder extends Binder {
         TemperatureSensorService getService() {
             return TemperatureSensorService.this;
         }
 
-    }
-
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
     }
 }
