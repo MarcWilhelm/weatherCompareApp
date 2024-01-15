6b16871e2fb849f173bfd626b82f098d00b1bda5
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2024-01-15 14:32:19 +0100

    added function to ask for gps

--- app/src/main/java/dev/marc/m335/weathercompareapp/MainActivity.java
+++ app/src/main/java/dev/marc/m335/weathercompareapp/MainActivity.java
@@ -1,29 +1,43 @@
 package dev.marc.m335.weathercompareapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.health.connect.datatypes.ExerciseRoute;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    String latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button nextBtn = (Button) findViewById(R.id.nextButton);
         TextInputLayout nameInput = (TextInputLayout) findViewById(R.id.name);
         nameInput.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(charSequence.toString().trim().length()==0){
+                if (charSequence.toString().trim().length() == 0) {
                     nextBtn.setEnabled(false);
                 } else {
                     nextBtn.setEnabled(true);
@@ -33,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
@@ -43,17 +56,57 @@ public class MainActivity extends AppCompatActivity {
         });
 
         nextBtn.setEnabled(true);
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, CompareActivity.class);
-                i.putExtra("name", "Marc");
-                startActivity(i);
+              //  if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+               // OnGPS();
+             //   } else {
+                  getLocation();
+              //  }
+                //  Intent i = new Intent(MainActivity.this, CompareActivity.class);
+                //   i.putExtra("name", "Marc");
+                //   startActivity(i);
 
             }
+
         });
+    }
+
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", (dialog, which) -> startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
 
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+                System.out.println("Your Location: " + " " + "Latitude: " + latitude + " " + "Longitude: " + longitude);
+            } else {
+                System.out.println("Unable to find location.");
+            }
+        }
     }
 }
